b55c9f94f225f7b88a42f4657620930c37cbb7bb
Kasey McCormick <mccormick.kasey@gmail.com>
2015-06-25 11:26:00 -0500

    making save work for timeframes

--- controllers/timeframes.rb
+++ controllers/timeframes.rb
@@ -5,11 +5,13 @@ end
 
 get '/timeframes/save' do
   
-  frame = TimeFrame.find(params["id"].to_i)
-  new_range = params["range"]
-  frame.range = new_range
-  frame.save
-
+  # frame = TimeFrame.find(params["id"].to_i)
+  # new_range = params["range"]
+  # frame.range = new_range
+  # frame.save
+  new_range = TimeFrame.new(params["range"])
+  new_range.save
+ 
   @new_timeframe = true  
   #update the database then send them to
   erb :"timeframes/add_timeframe_form"
