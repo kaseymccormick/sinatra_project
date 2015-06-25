daa9437a257757f45601d8399e1c2bfd7ddc53ce
Kasey McCormick <mccormick.kasey@gmail.com>
2015-06-25 12:15:49 -0500

    fixing ages table column name

--- database_setup.rb
+++ database_setup.rb
@@ -3,7 +3,7 @@ CONNECTION = SQLite3::Database.new("availability.db")
 
 # Make the tables.
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS responders (id INTEGER PRIMARY KEY, e_mail TEXT, age_id INTEGER);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS ages (id INTEGER PRIMARY KEY, range TEXT);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS ages (id INTEGER PRIMARY KEY, group TEXT);")
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS time_frames (id INTEGER PRIMARY KEY, range TEXT);")
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS days (id INTEGER PRIMARY KEY, date TEXT);")
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS available_times (responders_id INTEGER, timeframes_id INTEGER, days_id INTEGER);")
