9fd0b84ed7ba40d30a922b72665773b0e207ea9c
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-09 23:05:30 -0500

    removing links adding menu
    had admin link at the bottom of views, because there's a menu now it's not needed

--- public/global.js
+++ public/global.js
@@ -1,14 +1,16 @@
 // menue functionality on mobile//
 function showMenu(){
-   document.getElementById("menu").style.top = 0 + "px";   
+   document.getElementById("menu").className = " "; 
+   // document.getElementById("haze").className = "screen";
 }
 
 function hideMenu(){
-  document.getElementById("menu").style.top = "-" + 300 + "px";  
+  document.getElementById("menu").className = "hide";
+  // document.getElementById("haze").className = " ";
 }
 
 hideMenu();
 
-document.getElementById("bar1").onclick = showMenu;
+document.getElementById("adminicon").onclick = showMenu;
 
-document.getElementById("hamburger").ondblclick = hideMenu;
+document.getElementById("adminicon").ondblclick = hideMenu;
