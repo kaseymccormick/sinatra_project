572b65031499c73957a0d4f706444307f45f8aed
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-17 11:03:18 -0500

    deploy setup

--- app.rb
+++ app.rb
@@ -8,9 +8,29 @@ require "sinatra/reloader"
 require"active_support"
 require"active_support/inflector"
 
+#TODO
+configure :development do
+  require "sqlite3"
+  
+  ActiveRecord::Base.establish_connection(adapter:'sqlite3' database...)
+end
 
-#sql/ database
-require "sqlite3"
+configure :production do 
+  gem "pg"
+   
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
+#database setup
 require_relative "database_setup.rb"
 
 #models
