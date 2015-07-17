3130c2ccdb873112a06045ef935401b7c6598184
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-17 13:04:36 -0500

    starting to add active record after heroku setup

--- app.rb
+++ app.rb
@@ -45,3 +45,17 @@ require_relative "controllers/responders.rb"
 require_relative "controllers/timeframes.rb"
 require_relative "controllers/days.rb"
 require_relative "controllers/available_times.rb"
+
+require "pry"
+require "active_record"
+require "sqlite3"
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'your_database.db')
+
+# So that ActiveRecord explains the SQL it's running in the logs.
+ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
+
+# Models
+require_relative "models/task.rb"
+
+binding.pry
